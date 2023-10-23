7ff49899974bde882f9de936c7927d0af25ef2a1
javs099 <javs_adacer@hotmail.com>
2023-10-23 15:40:54 -0600

    semana uno jac

---
+++ tipos-primitivos/src/common/Principal.java
@@ -0,0 +1,32 @@
+package common;
+
+public class Principal {
+
+	
+	public static void main(String[] args) {
+		System.out.println("esto es una prueba");
+	}
+	
+	
+	// Datos primitivos
+	//Enteros
+	byte b = 5;
+	short s = 10;
+	int i = 15;
+	long l = 999999999999l;
+	
+	//Decimales
+	float f = 25.68f;
+	double d = 7985.245;
+	
+	// caracater y boleanos
+	char c = 'c';
+	boolean flag = true;
+	
+	
+	//Wrappers
+
+	
+	
+	
+}
