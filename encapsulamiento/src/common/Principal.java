42c55c1b0cc1088230567d81d95fa59b7a65414c
Jesus Reyes <jesusreyesh10@gmail.com>
2023-10-24 16:45:09 -0600

    semana dos encapsulamiento jrh

---
+++ encapsulamiento/src/common/Principal.java
@@ -0,0 +1,45 @@
+package common;
+
+public class Principal {
+
+	private String variable1;
+	protected String variable2;
+	public String variable3;
+	
+	public Principal() {
+		super();
+	}
+
+	public Principal(String variable1, String variable2, String variable3) {
+		super();
+		this.variable1 = variable1;
+		this.variable2 = variable2;
+		this.variable3 = variable3;
+	}
+
+	public String getVariable1() {
+		return variable1;
+	}
+
+	public void setVariable1(String variable1) {
+		this.variable1 = variable1;
+	}
+
+	public String getVariable2() {
+		return variable2;
+	}
+
+	public void setVariable2(String variable2) {
+		this.variable2 = variable2;
+	}
+
+	public String getVariable3() {
+		return variable3;
+	}
+
+	public void setVariable3(String variable3) {
+		this.variable3 = variable3;
+	}
+	
+	
+}
